4012b4b10bc9b896b12a6ee4887ecf775ea1a6be
mila-cybersecurity <milacyber@protonmail.com>
2024-10-24 11:15:33 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function openFolder(folderId) {
+    // Muestra la ventana asociada al folder
+    document.getElementById(folderId).style.display = 'block';
+}
+
+function closeWindow(windowId) {
+    // Oculta la ventana asociada
+    document.getElementById(windowId).style.display = 'none';
+}
