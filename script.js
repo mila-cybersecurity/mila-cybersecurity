7563660486a9053795bb375271e9232465790991
mila-cybersecurity <milacyber@protonmail.com>
2024-10-24 12:05:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ function openWindow(windowId) {
     centerWindow(windowElement); // Centra la ventana cuando se abre
 }
 
-// Función para cerrar una ventana
-function closeWindow(windowId) {
-    document.getElementById(windowId).style.display = 'none';
-}
-
 // Función para centrar la ventana al abrirla
 function centerWindow(windowElement) {
     windowElement.style.top = "50%";
@@ -17,41 +12,11 @@ function centerWindow(windowElement) {
     windowElement.style.transform = "translate(-50%, -50%)";
 }
 
-// Función para arrastrar ventanas
-function makeDraggable(element) {
-    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    element.querySelector('.window-header').onmousedown = dragMouseDown;
-
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        element.style.top = (element.offsetTop - pos2) + "px";
-        element.style.left = (element.offsetLeft - pos1) + "px";
-        element.style.transform = "none"; // Desactiva la transformación para arrastrar
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
+// Modificar la función de apertura de ventana para ajustar el tamaño de "Mis Documentos"
+function openDocumentsWindow() {
+    var windowElement = document.getElementById('myDocuments');
+    windowElement.style.display = 'block';
+    windowElement.style.width = "500px"; // Ajustar el ancho
+    windowElement.style.height = "400px"; // Ajustar la altura
+    centerWindow(windowElement);
 }
-
-// Hacer todas las ventanas movibles
-document.addEventListener("DOMContentLoaded", function() {
-    makeDraggable(document.getElementById('myDocuments'));
-    makeDraggable(document.getElementById('aboutMe'));
-    makeDraggable(document.getElementById('readme'));
-});
