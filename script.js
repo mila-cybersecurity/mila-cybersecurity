a6cd56bd69865b07817b47f85287106437252649
mila-cybersecurity <milacyber@protonmail.com>
2024-10-24 11:53:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // Función para abrir una ventana
 function openWindow(windowId) {
-    document.getElementById(windowId).style.display = 'block';
+    var windowElement = document.getElementById(windowId);
+    windowElement.style.display = 'block';
+    centerWindow(windowElement); // Centra la ventana cuando se abre
 }
 
 // Función para cerrar una ventana
 function closeWindow(windowId) {
     document.getElementById(windowId).style.display = 'none';
+}
+
+// Función para centrar la ventana al abrirla
+function centerWindow(windowElement) {
+    windowElement.style.top = "50%";
+    windowElement.style.left = "50%";
+    windowElement.style.transform = "translate(-50%, -50%)";
 }
 
 // Función para arrastrar ventanas
@@ -31,6 +40,7 @@ function makeDraggable(element) {
         pos4 = e.clientY;
         element.style.top = (element.offsetTop - pos2) + "px";
         element.style.left = (element.offsetLeft - pos1) + "px";
+        element.style.transform = "none"; // Desactiva la transformación para arrastrar
     }
 
     function closeDragElement() {
